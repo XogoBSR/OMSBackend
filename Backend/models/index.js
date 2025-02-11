d66d4ea62c4294b2b567a02b73cb4ac7b37e8763
XogoBSR <xogostudiosunityairm1.04@gmail.com>
2025-02-11 13:39:43 +0530

    updated

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -31,8 +31,11 @@ exports.connectDatabase = async () => {
         environment = process.env.PROD_MONGODB_URL;
     }
 
-    await mongoose.connect(environment, {
+    await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 10000, // 10 seconds timeout
+      })
+      .then(() => console.log("✅ MongoDB Connected Successfully"))
+      .catch(err => console.error("❌ MongoDB Connection Error:", err));
 }
